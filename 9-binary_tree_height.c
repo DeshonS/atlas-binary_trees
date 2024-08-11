ecce64aa1d9b41b83a6f8dae27a64ca0092b3dc0
DeshonS <deshonjewell@gmail.com>
2024-08-11 08:59:37 -0500

    done

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - opening of file
+ * @tree: pointer to the root of the tree
+ * Return: Height of the tallest side of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left, right;
+
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left == NULL || tree->right == NULL)
+{
+return (0);
+}
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+if (left > right)
+{
+return (left + 1);
+}
+if (right > left)
+{
+return (right + 1);
+}
+}
