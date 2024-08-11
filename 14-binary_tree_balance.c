c75046c099920fa23afa75798d60cc2a57de1f9c
DeshonS <deshonjewell@gmail.com>
2024-08-11 09:44:16 -0500

    added binary_tree_height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - opening of file
+ * @tree: pointer to the root of the tree
+ * Return: Height of the tallest side of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left, right;
+
+if (tree == NULL)
+{
+return (0);
+}
+left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+return (left > right ? left : right);
+}
+
+/**
  * binary_tree_balance - opening of file
  * @tree: pointer to root of tree
  * Return: factor balance of tree, or 0 for errors
