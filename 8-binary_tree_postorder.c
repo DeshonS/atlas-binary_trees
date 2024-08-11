d399b868461f214ad36563b76fc62b2bec60f37c
DeshonS <deshonjewell@gmail.com>
2024-08-11 05:21:29 -0500

    done

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * bianry_tree_postorder - opening of file
+ * @tree: pointer to root of tree
+ * @func: function to call data
+ * Return: None (void)
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL || func == NULL)
+{
+return;
+}
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
