6e31524d1bd6a022532f9c0b1aa688fc91563c5c
DeshonS <deshonjewell@gmail.com>
2024-08-11 09:41:07 -0500

    done

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - opening of file
+ * @tree: pointer to root of tree
+ * Return: factor balance of tree, or 0 for errors
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (NULL);
+    }
+    return (int)(binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
