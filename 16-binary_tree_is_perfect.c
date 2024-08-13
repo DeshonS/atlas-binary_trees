e9541ff8741f03e499e01e17dcba325389d737be
DeshonS <deshonjewell@gmail.com>
2024-08-12 22:08:56 -0500

    formatted and added prototype

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,15 +8,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left, right;
+size_t left, right;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-    right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
-    return (left > right ? left : right);
+if (tree == NULL)
+{
+return (0);
+}
+left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+return (left > right ? left : right);
 }
 
 /**
@@ -27,11 +27,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+if (tree == NULL)
+{
+return (0);
+}
+return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
 
 /**
@@ -42,14 +42,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t height, size, expected_size;
+size_t height, size, expected_size;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    height = binary_tree_height(tree);
-    size = binary_tree_size(tree);
-    expected_size = (1 << (height + 1)) - 1;
-    return (size == expected_size);
+if (tree == NULL)
+{
+return (0);
+}
+height = binary_tree_height(tree);
+size = binary_tree_size(tree);
+expected_size = (1 << (height + 1)) - 1;
+return (size == expected_size);
 }
